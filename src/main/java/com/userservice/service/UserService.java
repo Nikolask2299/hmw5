5d85a03d63f14f1ae3b324ca7a2a89b0a2bd2c69
Nikolask2299 <nikolasff889@yandex.ru>
2025-11-16 00:45:04 +0400

    upd

---
+++ src/main/java/com/userservice/service/UserService.java
@@ -0,0 +1,64 @@
+package com.userservice.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.userservice.dto.CreateUserRequest;
+import com.userservice.dto.UserDto;
+import com.userservice.entity.User;
+import com.userservice.exception.UserNotFoundException;
+import com.userservice.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDto createUser(CreateUserRequest request){
+        if (userRepository.findByEmail(request.email()).isPresent()) {
+            throw new RuntimeException("User with " + request.email() + " already exists");
+        }
+
+        User user = new User(request.name(), request.email(), request.age());
+        User savedUser = userRepository.save(user);
+        return new UserDto(savedUser);
+    }
+    
+    public UserDto getUser(Long id){
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new UserNotFoundException("User with id " + id + " not found")
+        );
+        return new UserDto(user);
+    }
+
+    public List<UserDto> getAllUsers(){
+        return userRepository.findAll().stream().map(UserDto::new).toList();
+    }
+
+    public void deleteUser(Long id){
+        if (userRepository.existsById(id)){
+            userRepository.deleteById(id);
+        } else {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+    }
+
+    public UserDto updateUser(Long id, CreateUserRequest request){
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new UserNotFoundException("User with id " + id + " not found")
+        );
+
+        user.setName(request.name());
+        user.setEmail(request.email());
+        user.setAge(request.age());
+
+        User updatedUser = userRepository.save(user);
+        return new UserDto(updatedUser);
+    }
+
+
+}
